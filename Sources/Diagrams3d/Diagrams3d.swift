eca683a12d76fc850c7c877e4b163351853e6f50
Olesia <85791382+caleyass@users.noreply.github.com>
2024-11-08 23:38:38 +0200

    init commit

---
+++ Sources/Diagrams3d/Diagrams3d.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class Diagrams3d {
+    public init() {}
+    public func hello() {
+        print("Hello")
+    }
+}
